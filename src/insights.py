d97ca250e7cb6abc6836f256ae34f6d8b6a1957c
ZekaEu <zeka.surf@hotmail.com>
2022-07-09 06:51:11 -0300

    Requisito 9

--- src/insights.py
+++ src/insights.py
@@ -62,18 +62,11 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    jobs_filtered_by_salary_range_list = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                jobs_filtered_by_salary_range_list.append(job)
+        except ValueError:
+            print("UEPAAAA!")
+    return jobs_filtered_by_salary_range_list
