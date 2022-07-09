066893e828e45c00ac5f01ca1337240d40cf7d7a
ZekaEu <zeka.surf@hotmail.com>
2022-07-09 06:32:18 -0300

    Requisito 7

--- src/insights.py
+++ src/insights.py
@@ -25,21 +25,10 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filtered_industry_list = [
+        job for job in jobs if job["industry"] == industry
+    ]
+    return filtered_industry_list
 
 
 def get_max_salary(path):
