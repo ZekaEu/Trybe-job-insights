e2c994f90659f54f3fff676089ce46304323fb07
ZekaEu <zeka.surf@hotmail.com>
2022-07-09 06:57:53 -0300

    Requisito 10?

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences('src/jobs.csv', 'python') == 1639
+    assert count_ocurrences('src/jobs.csv', 'PyThOn') == 1639
+    assert count_ocurrences('src/jobs.csv', 'JAVASCRIPT') == 122
+    assert count_ocurrences('src/jobs.csv', 'jAvAsCrIpT') == 122
