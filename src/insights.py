8bb916bb73efe1cd9a95e4ed90558a251edc77fd
ZekaEu <zeka.surf@hotmail.com>
2022-07-09 06:25:28 -0300

    Requisito 5

--- src/insights.py
+++ src/insights.py
@@ -65,21 +65,12 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    job_list = read(path)
+    salary_list = []
+    for job in job_list:
+        if job["min_salary"].isdigit():
+            salary_list.append(int(job["min_salary"]))
+    return min(salary_list)
 
 
 def matches_salary_range(job, salary):
